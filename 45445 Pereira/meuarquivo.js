0f3573e789bbd5b4c3769fb797bfb3fb50f663a8
Yangmape <134799815+Mateuspsantos1@users.noreply.github.com>
2023-06-01 22:14:52 -0300

    Add files via upload

---
+++ 45445 Pereira/meuarquivo.js
@@ -0,0 +1,21 @@
+let senha = prompt("Inserir sua senha");
+
+while(senha != "ESC" ){
+   switch (senha) {
+       case "12345":
+            alert("Senha correta, seja bem-vindo");
+            break;
+            default:
+           alert("SENHA INVALIDA")
+           break;
+   }
+   senha = prompt("Inserir sua senha")
+}
+
+
+
+
+
+
+    
+       
